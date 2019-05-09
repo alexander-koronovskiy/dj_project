ed5848b3b0adf6e0a07efc6d0371b775cdd0a935
alexander <alexander.koronovskiy@gmail.com>
2019-05-09 21:51:09 +0400

    before render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from .models import Question
 
 
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
